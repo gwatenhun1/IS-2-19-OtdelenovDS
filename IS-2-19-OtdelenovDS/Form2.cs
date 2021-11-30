98ffad5e2f07f950a381193df70bdccb3f7ce717
Дмитрий Отделенов <otdelenovsd@mail.ru>
2021-11-30 19:22:32 +0500

    Сделал 2 задание

--- IS-2-19-OtdelenovDS/Form2.cs
+++ IS-2-19-OtdelenovDS/Form2.cs
@@ -7,14 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace IS_2_19_OtdelenovDS
 {
     public partial class Form2 : Form
     {
-        public Form2()
+        static class DBUtils
         {
-            InitializeComponent();
+            public static string GetDBConnection()
+            {
+                string host = "caseum.ru";
+                string port = "33333";
+                string database = "db_test";
+                string username = "test_user";
+                string password = "test_pass";
+                string connString = $"server={host};port={port};user={username};database={database};password={password};";
+                return connString;
+            }
+        }
+        MySqlConnection conn = new MySqlConnection(DBUtils.GetDBConnection());
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                MessageBox.Show("Удача");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Не повезло: " + ex);
+            }
+            finally
+            {
+
+            }
         }
     }
 }
